b7883a735907c11ac004e3940dee91c9b770896a
WilsonHuang <hjn201@gmail.com>
2016-06-28 22:13:38 +0800

    2016/06/28練習

--- ASP.NET小練習/CH5/ch5_Practice_3.aspx
+++ ASP.NET小練習/CH5/ch5_Practice_3.aspx
@@ -4,18 +4,27 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title></title>
 </head>
 <body>
     <form id="form1" runat="server">
-    <div>
-        <asp:DropDownList ID="DropDownList1" runat="server" >
-            <asp:ListItem>網球</asp:ListItem>
-            <asp:ListItem>桌球</asp:ListItem>
-            <asp:ListItem>羽毛球</asp:ListItem>
-        </asp:DropDownList>
-    </div>
+        <div>
+            請選擇總類：
+            <asp:DropDownList ID="dropDownList1" runat="server" Width="150px" AutoPostBack="True" OnSelectedIndexChanged="dropDownList1_SelectedIndexChanged" >
+                <asp:ListItem Selected="True" Value="0">請選擇總類</asp:ListItem>
+                <asp:ListItem Value="1">網球</asp:ListItem>
+                <asp:ListItem Value="2">桌球</asp:ListItem>
+                <asp:ListItem Value="3">羽毛球</asp:ListItem>
+            </asp:DropDownList>
+            &nbsp;&nbsp;&nbsp;請選擇品牌：
+            <asp:DropDownList ID="dropDownList2" runat="server" Width="150px" AutoPostBack="True" OnSelectedIndexChanged="dropDownList2_SelectedIndexChanged" /><br />
+            <br />
+            總類：<asp:Label ID="label_category" runat="server" ></asp:Label><br />
+            品牌：<asp:Label ID="label_brand" runat="server" ></asp:Label><br />
+
+
+        </div>
     </form>
 </body>
 </html>
--- ASP.NET小練習/CH5/ch5_Practice_3.aspx.cs
+++ ASP.NET小練習/CH5/ch5_Practice_3.aspx.cs
@@ -7,8 +7,50 @@ using System.Web.UI.WebControls;
 
 public partial class ch5_Practice3 : System.Web.UI.Page
 {
+    string[] tennis = { "請選擇品牌", "Wilson", "Dunlop", "Fischer", "Yonex" };
+    string[] tableTennis = { "請選擇品牌", "TSP", "Nittaku", "DONIC", "Butterfly" };
+    string[] badminton = { "請選擇品牌", "YONEX", "VICTOR", "FLEET", "KENDELE" };
+    string[] nothing = { "請先選擇總類" };
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        
+    }
 
+    protected void dropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        switch (dropDownList1.SelectedItem.Value)
+        {
+            case "1":
+                addItem(tennis);
+                break;
+            case "2":
+                addItem(tableTennis);
+                break;
+            case "3":
+                addItem(badminton);
+                break;
+            default:
+                addItem(nothing);
+                label_category.Text = "";
+                label_brand.Text = "";
+                break;
+        }
+    }
+
+    protected void dropDownList2_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        label_category.Text = dropDownList1.SelectedItem.Text;
+        label_brand.Text = dropDownList2.SelectedItem.Text;
+    }
+
+    private void addItem(string[] category)
+    {
+        dropDownList2.Items.Clear();
+
+        for (int i = 0; i < category.Length; i++)
+        {
+            dropDownList2.Items.Add(category[i]);
+        }
     }
 }
